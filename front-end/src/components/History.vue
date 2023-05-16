2c592f8bbd00013e12c7370d63b1855624fb5df8
Taysir96 <91126653+Taysir96@users.noreply.github.com>
2023-05-16 19:34:17 +0200

    done history

--- front-end/src/components/History.vue
+++ front-end/src/components/History.vue
@@ -43,8 +43,8 @@ export default {
                 <tbody>
                     <tr v-for="history in this.history" :key="history.id">
                         <td>{{ history.id }}</td>
-                        <td>{{ history.userId }}</td>
-                        <td>{{ history.itemId }}</td>
+                        <td>{{ history.user.email }}</td>
+                        <td>{{ history.item.name }}</td>
                         <td>{{ history.description }}</td>
                         <td>{{ history.startDate }}</td>
                         <td>{{ history.endDate }}</td>
